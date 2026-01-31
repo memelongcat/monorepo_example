cd6b50e777b59877e25a55346601e859dcd86613
memelongcat <45173753+memelongcat@users.noreply.github.com>
2026-01-31 18:54:47 +0700

    backend add orm

--- back/src/config.py
+++ back/src/config.py
@@ -1,11 +1,24 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+class DBSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        extra='ignore',
+        env_file='.env'
+    ) 
+    MYSQL_USER: str 
+    MYSQL_PASSWORD: str 
+    MYSQL_DATABASE: str
+    MYSQL_PORT: str
+    MYSQL_ADDRESS: str
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         extra='ignore',
         env_file='.env'
     )
     RUN_MODE: str = 'debug'
+    DB: DBSettings = DBSettings()
 
 settings = Settings()
 
--- back/src/main.py
+++ back/src/main.py
@@ -1,12 +1,57 @@
+
 from fastapi import FastAPI
 
 from src.config import settings
+
+from sqlalchemy import create_engine
+
+from sqlalchemy.orm import Session
+
+from sqlalchemy import text
 
 if settings.RUN_MODE != 'prod':
     app = FastAPI(docs_url='/')
 else:
     app = FastAPI()
     print('prod')
+
+
+@app.get('/init-db')
+def init_db():
+    engine = create_engine(f"mysql+pymysql://{settings.DB.MYSQL_USER}:{settings.DB.MYSQL_PASSWORD}@{settings.DB.MYSQL_ADDRESS}:{settings.DB.MYSQL_PORT}/{settings.DB.MYSQL_DATABASE}", echo=True)
+    with engine.connect() as connection:
+        result = connection.execute(text(
+            "CREATE TABLE IF NOT EXISTS test ( \
+                id INT AUTO_INCREMENT PRIMARY KEY, \
+                num integer, \
+                data varchar(255) \
+            );" 
+        ))    
+        connection.execute(text('INSERT INTO test (num, data) VALUES (:num, :data)').bindparams(num=100, data='abc'))
+        a = str([i for i in connection.execute(text('SELECT * FROM test')).all()])
+        return a
+
+
+@app.get('/add-data')
+def init_db():
+    engine = create_engine(f"mysql+pymysql://{settings.DB.MYSQL_USER}:{settings.DB.MYSQL_PASSWORD}@{settings.DB.MYSQL_ADDRESS}:{settings.DB.MYSQL_PORT}/{settings.DB.MYSQL_DATABASE}", echo=True)
+    with engine.connect() as connection:
+        connection.execute(text('INSERT INTO test (num, data) VALUES (:num, :data)').bindparams(num=100, data='abc'))
+        a = str([i for i in connection.execute(text('SELECT * FROM test ORDER BY test.id')).all()])
+        return a
+
+
+
+#@app.get('/getdb')
+#def get_db_creds():
+#    tuple = (
+#        settings.DB.MYSQL_ADDRESS,
+#        settings.DB.MYSQL_USER,
+#        settings.DB.MYSQL_PASSWORD,
+#        settings.DB.MYSQL_DATABASE,
+#        settings.DB.MYSQL_PORT,
+#            )
+#    return tuple
 
 @app.get('/42')
 def get_42():
